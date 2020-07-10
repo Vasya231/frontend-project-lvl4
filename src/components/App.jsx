a1bee7b068231b27c092128650295607bdf831ba
Vasya231 <gg231589@gmail.com>
2020-07-10 19:59:25 +0300

    refactor: continue getting rid of custom styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import NewMessageForm from 'features/messages/NewMessageForm';
 
 const App = () => (
   <>
-    <div className="row h-100 pb-3 d-flex">
+    <div className="row h-100 pb-3 d-flex flex-column flex-md-row">
       <ChannelsWindow />
       <div className="col-md-8 flex-grow-1 mh-100 d-flex flex-column flex-grow-1 justify-content-between chat-window mb-1">
         <MessageList />
