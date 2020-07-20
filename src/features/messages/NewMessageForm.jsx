2fee4a5f5c9eb98bc4c8276ef5ed854392930326
Vasya231 <gg231589@gmail.com>
2020-07-20 18:35:57 +0300

    refactor: change new message form to use useFormik

--- src/features/messages/NewMessageForm.jsx
+++ src/features/messages/NewMessageForm.jsx
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react';
 import { connect } from 'react-redux';
-import {
-  Formik, Form, Field,
-} from 'formik';
+import { useFormik } from 'formik';
 import Button from 'react-bootstrap/Button';
 import { useTranslation } from 'react-i18next';
 
@@ -22,40 +20,44 @@ const NewMessageForm = (props) => {
   const { username } = useContext(AppContext);
   const { t } = useTranslation();
   const { activeChannelId, openModal } = props;
+  const formik = useFormik({
+    initialValues: { text: '' },
+    validate: validateMessageText,
+    onSubmit: async (values, formikActions) => {
+      console.log(values);
+      const { setSubmitting, resetForm } = formikActions;
+      try {
+        await serverAPI.addNewMessage(values.text, username, activeChannelId);
+        resetForm();
+        setSubmitting(false);
+      } catch (error) {
+        openModal({
+          type: 'errorMessage',
+          modalProps: {
+            errorMessage: t('errors.network'),
+          },
+        });
+      }
+    },
+  });
+  const {
+    values, dirty, isValid, isSubmitting, handleChange, errors, handleSubmit,
+  } = formik;
   return (
-    <Formik
-      initialValues={{ text: '' }}
-      validate={validateMessageText}
-      onSubmit={async (values, formikActions) => {
-        const { setSubmitting, resetForm } = formikActions;
-        try {
-          await serverAPI.addNewMessage(values.text, username, activeChannelId);
-          resetForm();
-          setSubmitting(false);
-        } catch (error) {
-          openModal({
-            type: 'errorMessage',
-            modalProps: {
-              errorMessage: t('errors.network'),
-            },
-          });
-        }
-      }}
-    >
-      {({
-        isSubmitting, errors, isValid, dirty,
-      }) => (
-        <Form>
-          <div className="input-group mw-100">
-            <Field type="text" name="text" autoComplete="off" className="form-control" />
-            <Button type="submit" disabled={isSubmitting || !dirty || !isValid} variant="primary" className="ml-1 d-none d-md-block">
-              {t('chatWindow.submit')}
-            </Button>
-            <div>{errors.submit}</div>
-          </div>
-        </Form>
-      )}
-    </Formik>
+    <form className="input-group mw-100" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="text"
+        autoComplete="off"
+        className="form-control"
+        onChange={handleChange}
+        value={values.text}
+      />
+      <Button type="submit" disabled={isSubmitting || !dirty || !isValid} variant="primary" className="ml-1 d-none d-md-block">
+        {t('chatWindow.submit')}
+      </Button>
+      <div>{errors.submit}</div>
+    </form>
   );
 };
 
