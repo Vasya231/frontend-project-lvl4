1bf7cd65dcc1765bd8a3a41cc2d296c1b05ed448
Vasya231 <gg231589@gmail.com>
2020-07-23 15:04:20 +0300

    refactor(constants.js): remove default export

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,3 @@ export const channelNameMaxLength = 12;
 export const generalChannelId = 1;
 
 export const mobileWidthBreakpoint = 768;
-
-export default {
-  channelNameMaxLength, generalChannelId, mobileWidthBreakpoint,
-};
